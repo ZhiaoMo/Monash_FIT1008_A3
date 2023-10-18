069ddf59a04b549c64d1a893144077eaf19cec75
Zhiao Mo <mozhiao@iCloud.com>
2023-10-18 23:42:12 +1100

    feat: done

--- mode1.py
+++ mode1.py
@@ -4,25 +4,76 @@ from algorithms.mergesort import mergesort
 
 class Mode1Navigator:
     """
-    Student-TODO: short paragraph as per https://edstem.org/au/courses/12108/lessons/42810/slides/294117
+    This class used to find the best order islands to make money. My strategy is using mergesort the key is marine/money, 
+    for sort the island from highest efficiency to lowest efficiency. Then, I will select the island from the highest to lowest
+    untill run out the crew.
+
+    Data Structures used:
+    - List: To store the islands and their marine-to-money ratio.
+    
+    Example:
+        Island("A", 400, 100)
+        Island("B", 300, 150)
+        Island("C", 100, 5)
+        Island("D", 350, 90)
+        Island("E", 300, 100)
+        - after sort:
+        Island("C", 100, 5)
+        Island("A", 400, 100)
+        Island("D", 350, 90)
+        Island("E", 300, 100)
+        Island("B", 300, 150)
+
+        if crew = 200
+        we can get 100 + 400 + 350 + 19.4444 = 869.4444
+        much more than before sort 400 + 300*(100/150) = 600
     """
 
     def __init__(self, islands: list[Island], crew: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Algorithm used:
+        - Merge Sort: To sort the islands from highest efficiency to lowest efficiency with Worst Case O(NlogN) complexity.
+
+        Complexity Analysis:
+        - Best Case and Worst case: O(NlogN) merge sort have O(NlogN) complexity, and the creat the list of marine-to-money ratio
+            have O(N) complexity. So the total complexity is O(NlogN).
         """
         
-
         self.islands = mergesort(islands, key=lambda x: x.marines/x.money)
         self.islands_ratio  = [i.marines/i.money for i in self.islands]
         self.crew = crew
 
     def select_islands(self) -> list[tuple[Island, int]]:
         """
-        Student-TODO: Best/Worst Case
+        Data Structures used:
+        - List: To store the instance of islands.
+        - List: To store the sekected isalnd and the crew sent to the island.
+        Example:
+            Island("A", 400, 100)
+            Island("B", 300, 150)
+            Island("C", 100, 5)
+            Island("D", 350, 90)
+            Island("E", 300, 100)
+            - after sort:
+            Island("C", 100, 5)
+            Island("A", 400, 100)
+            Island("D", 350, 90)
+            Island("E", 300, 100)
+            Island("B", 300, 150)
+
+            if crew = 200
+            we can get 100 + 400 + 350 + 19.4444 = 869.4444
+            much more than before sort 400 + 300*(100/150) = 600
+
+        Complexity Analysis:
+        - Best Case: O(logN) N is the number of islands, becuase crew is constant, every ieration
+            will reduce the number of islands possible to select by nearlly half. The conplexity 
+            update the list all with O(1) complexity.
+        - Worst Case: O(N) N is the number of islands, if the crew is very large, it have to iterate 
+            all the islands. The conplexity update the list all with O(1) complexity.
         """
         if len(self.islands) == 0:
-            return []
+            raise Exception("No islands added!")
         crew_left = self.crew
         list_select_islands = []
         for island in self.islands:
@@ -39,7 +90,15 @@ class Mode1Navigator:
 
     def select_islands_from_crew_numbers(self, crew_numbers: list[int]) -> list[float]:
         """
-        Student-TODO: Best/Worst Case
+        Data Structures used:
+        - List: To store the instance of islands.
+        - List: To store the money each number of crew makes.
+        Complexity Analysis:
+        - Best Case: O(C * logN) C is the number of crew, N is the number of islands, becuase crew is constant, every ieration
+            will reduce the number of islands possible to select by nearlly half. Similar as select_islands. The conplexity 
+            update the list all with O(1) complexity.
+        - Worst Case: O(C * N) C is the number of crew, N is the number of islands, if the crew is very large, it have to 
+            iterate all the islands. The conplexity update the list all with O(1) complexity.
         """
         money_maked = []
         for index, crew_left in enumerate(crew_numbers):
@@ -56,7 +115,13 @@ class Mode1Navigator:
 
     def update_island(self, island: Island, new_money: float, new_marines: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Data Structures used:
+        - List: To store the instance of islands.
+        Algorithm used:
+        - Binary Search: To find the index of the island in the list with Worst Case O(logN) complexity.
+        Complexity Analysis:
+        - Best Case and Worst case: O(logN) N is the number of islands, get the index used binary search with O(logN) complexity.
+            the update the island with O(1) complexity.
         """
         index = binary_search(self.islands_ratio, island.marines/island.money)
         self.islands[index].money = new_money
--- mode2.py
+++ mode2.py
@@ -1,24 +1,72 @@
 from island import Island
+from algorithms.mergesort import mergesort
 
 class Mode2Navigator:
     """
-    Student-TODO: short paragraph as per https://edstem.org/au/courses/12108/lessons/42810/slides/294117
+    This class used to simulate a "sandbox" lets a few pirates plunder an island, and returns the results.
+    the key strategy is same as mode1. it will sort the island one tiem before all pirates select the island.
+
+    Data Structures used:
+    - List: To store the instance of islands.
     """
 
     def __init__(self, n_pirates: int) -> None:
         """
-        Student-TODO: Best/Worst Case
+        Data Structures used:
+        - List: To store the instance of islands.
+
+        Complexity Analysis:
+        - Best Case and Worst case: O(1) no loop or recursion.
         """
-        raise NotImplementedError()
+        self.priates = n_pirates
+        self.islands = []
 
     def add_islands(self, islands: list[Island]):
         """
-        Student-TODO: Best/Worst Case
+        Data Structures used:
+        - List: To store the instance of islands.
+
+        Complexity Analysis:
+        - Best Case and Worst case: O(N) N is the length of islands.
         """
-        raise NotImplementedError()
+        for i in islands:
+            self.islands.append(i)
 
     def simulate_day(self, crew: int) -> list[tuple[Island|None, int]]:
         """
-        Student-TODO: Best/Worst Case
+        Data Structures used:
+        - List: To store the instance of islands.
+        - List: To store the seleted isalnd and the crew sent to the island.
+
+        Complexity Analysis:
+        - Best Case and Worst case: O(NlogN) merge sort have O(NlogN) complexity, and the creat the list of marine-to-money ratio
+            have O(N) complexity. And also have O(C) C is the number of crew, but the rate of increase less than O(NlogN)
+            So the total complexity is O(NlogN).
         """
-        raise NotImplementedError()
+        results = []
+        islands = mergesort(self.islands, key=lambda x: x.marines/x.money)
+        self.islands_ratio  = [i.marines/i.money for i in self.islands]
+        for _ in range(self.priates):
+
+            
+            if len(islands) != 0:
+                island:Island = islands[0]
+
+            elif island.marines <= crew and island.money > 2 * crew:
+                # island is not reamin money
+                results.append((island, island.marines))
+                island.marines = 0
+                island.money = 0
+            
+            elif island.marines > crew and island.money*(crew/island.money) > 2 * crew:
+                # island is reamin money
+                results.append((island, crew))
+                island.marines -= crew
+                island.money -= island.money*(crew/island.marines)
+            else:
+                # no island be selected today
+                results.append((None, 0))
+                continue
+                
+            self.islands = islands
+        return results
